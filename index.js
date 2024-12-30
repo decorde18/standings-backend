9b34dd65e2b4323ff6e5f99e4862fb1e4f15ab67
David Cordero de Jesus <decorde@yahoo.com>
2024-12-30 14:45:52 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+require("dotenv").config(); // Load .env variables
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const mysql = require("mysql2");
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.DB_PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Soccer Standings API is running");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Database connection error:", err);
+  } else {
+    console.log("Database connected");
+  }
+});
+
+app.get("/teams", (req, res) => {
+  db.query("SELECT * FROM Teams", (err, results) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.json(results);
+    }
+  });
+});
